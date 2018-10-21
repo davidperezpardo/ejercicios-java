f3bc4928ab09de809703dad9fafba732049c4958
david perez pardo <perpar.david@gmail.com>
2018-10-21 21:40:15 +0200

    update exercise

--- tema5ejercicios/ejercicio25Tema5.java
+++ tema5ejercicios/ejercicio25Tema5.java
@@ -7,16 +7,17 @@ public class ejercicio25Tema5 {
     
     Scanner s = new Scanner(System.in);
     System.out.println("INTRODUZCA NÚMERO");
-    long numIntro = s.nextInt();
+    long numIntro = s.nextLong();
     long numero = numIntro;
     long numReves = 0;
-    
     while (numero > 0) {
       numReves = (numReves*10)+(numero%10);     //con el modulo %10 obtenemos la unidad del nº introducido
       numero /=10;                             //dividimos entre 10 el nº introducido,dentro de otra variable
     }                                         //para desplazar la coma a la izquierda y obtener el siguiente nº
-    System.out.println(numReves);
-
-	}
+                                              /*numReves se multiplica por diez y se suma el  numero%10, 
+                                              * eso nos va acumulando los numeros del revés */
+    System.out.println("EL NÚMERO INTRODUCIDO "+numIntro+" AL REVÉS ES :"+numReves);  
+                                                                          
+  }
 }
 
